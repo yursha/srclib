321fb581b1514236ae81d809623844cbcd40a606
Alex Yursha <alexyursha@gmail.com>
2020-11-13 11:37:03 -1000

    Update docs

--- combinatorics/lib/collision_probability.h
+++ combinatorics/lib/collision_probability.h
@@ -1,7 +1,8 @@
 #include <stdint.h>
 
-// Computes a chance of at least one collision (eaual value) when we sample
-// `num_sampled` values in the domain of size `num_unique`.
+// Given `num_sampled` randomly generated values,
+//  where each value is a non-negative integer
+// less than `num_unique`, what is the probability that at least two of them are equal.
 //
 // This is a general case of a Birthday Problem.
 long double collision_probability(uint64_t num_unique, uint64_t num_sampled);
